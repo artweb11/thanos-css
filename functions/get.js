908a032372b5584c066a7edfc5081c7cfb1959a6
Sebastian Bratu <sebastian.bratu@squid40.com>
2020-08-24 19:46:53 +0300

    test deploy3

--- functions/get.js
+++ functions/get.js
@@ -3,14 +3,31 @@ const url = require('url');
 
 exports.handler = async ( event , context, callback ) => 
 { 
-    console.log('url: ', event.queryStringParameters.url );
-    getRequest( event.queryStringParameters.url, callback );
+    if (event.httpMethod === 'OPTIONS') {
+        preflight(callback);
+    } else {
+        console.log('url: ', event.queryStringParameters.url );
+        getRequest( event.queryStringParameters.url, callback );
+    }
+    
 }
 
-function getRequest(url, callback) {
+function preflight(callback) {
+    callback(null, {
+      statusCode: 204,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT',
+      },
+      body: {},
+    });
+  }
+
+function getRequest(urlStr, callback) {
     const clientReq = https.request(
       {
-        url,
+        url.parse( urlStr ),
         headers: {
           'Content-Type': 'application/json',
         },
