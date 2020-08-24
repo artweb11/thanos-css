97330aa63cea20ca89bfcf2122716846491deb67
Sebastian Bratu <sebastian.bratu@squid40.com>
2020-08-24 19:35:09 +0300

    test deploy

--- functions/get.js
+++ functions/get.js
@@ -1,7 +1,45 @@
-exports.handler = async ( event , context ) => 
+exports.handler = async ( event , context, callback ) => 
 { 
-    return { 
-        statusCode: 200, 
-        body: "Works" 
-    }; 
+    getRequest( event.queryStringParameters.url, function( err, resp ){
+        if( err ){ callback(null, {
+                statusCode: 200,
+                body: err
+            });
+        }
+
+        callback( null, resp );
+    } );
 }
+
+function getRequest(url, callback) {
+    const clientReq = https.request(
+      {
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      clientRes => {
+        clientRes.setEncoding('utf8');
+        let rawData = '';
+        clientRes.on('data', chunk => {
+          rawData += chunk;
+        });
+        clientRes.on('error', err => {
+          console.log('error here line 140', err);
+          callback(err.message);
+        });
+        clientRes.on('end', () => {
+          callback(null, {
+            statusCode: 200,
+            headers: {
+            //   'content-type': 'application/json',
+              'Access-Control-Allow-Origin': '*',
+            },
+            body: rawData,
+          });
+        });
+      }
+    );
+    clientReq.end();
+  }
