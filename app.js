c693153a01c91ff4b00b20905ca795380e4160aa
Sebastian Bratu <sebastian.bratu@squid40.com>
2020-08-25 17:06:27 +0300

    dissolve effect

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function(){
+(function(){    
 
     document.querySelector('#form').addEventListener('submit', function(e){
         e.preventDefault();
@@ -13,6 +13,7 @@
 
         /*
         document.querySelector('#site').innerHTML = '<iframe id="ifr" src="'+url+'" width="'+w+'px" height="100%" frameborder="0" onload="loadIFR()"></iframe>';*/
+        document.body.classList.add('loading');
 
         fetch( url, {
             method: "GET",
@@ -27,6 +28,11 @@
             img.src = src;
 
             img.onload = function(){
+                // var img = document.createElement('img');
+                // img.src = src;
+                // img.style.cssText = 'position: absolute;right:0;top:0;';
+                // document.querySelector('#clone').appendChild( img );
+
                 var c = document.createElement('canvas');
                 c.width = ~~(w/2);
                 c.height = h;
@@ -35,15 +41,22 @@
 
                 var ctx = c.getContext('2d');
                 ctx.drawImage( img, ~~(w/2), 0, w, h, 0, 0, w, h );
+
+                document.body.classList.remove('loading');
+                document.body.classList.add('loaded');
+                window.real.classList.add('halved');
+
+                setTimeout( function(){
+                    disintegrate( ctx, ~~(w/2), h, 4, 4 );
+                }, 1000 );
             }
 
             document.querySelector('#site').appendChild( img );
 
         
-            document.body.classList.add('loaded');
-            window.real.classList.add('halved');
+            
         }).catch( function( e ){
-            console.log( e );
+            alert('The site took more than 10 seconds to load. Please try another site.');
         });
         
 
@@ -55,7 +68,52 @@
         }
     }, false);
 
+    function disintegrate( ctx, w, h, cols, rows ){
+        var mx = Math.ceil(w/cols);
+        var my = Math.ceil(h/rows);
+        var cw = Math.ceil( w/mx );
+        var ch = Math.ceil( h/my );
+        var arr = [];
 
+        var imd = ctx.createImageData( cw, ch );
+        for (let i = 0; i < imd.data.length; i += 4) {
+            imd.data[i + 3] = 0;  // A value
+        }
+
+        for( var i=0; i<my; i++){
+            for( var j=0; j<mx; j++){
+                arr.push( {x: j*cw, y: i*ch, val: Math.random()*15, dissapeared: false });
+            }
+        }
+
+        requestAnimationFrame( anim );
+
+
+        function anim(){
+            // for( var i=0; i<200; i++){
+            //     var x = ~~( Math.random()*(w-5) );
+            //     var y = ~~( Math.random()*(h-5) );
+
+            //     ctx.fillRect( x, y, 5, 5 );
+            // }
+            for( var i in arr ){
+                arr[i].val -= 0.12;
+                if( arr[i].val < 1 ){
+                    //ctx.fillRect( arr[i].x, arr[i].y, cw, ch );
+                    ctx.putImageData( imd, arr[i].x, arr[i].y );
+                    arr[i].dissapeared = true;
+                }                
+            }
+            var ct =  arr.filter( function(i){ return !i.dissapeared }).length;
+            // console.log( ct );
+
+            if( ct > 0 ){
+                requestAnimationFrame( anim );
+            }
+
+        }
+        
+    }
     
 
 })();
